3f8629978fb1973346c3c99717c78915385bf554
hpjjphhpj <hpjjphhpj@gmail.com>
2021-01-20 21:15:11 +1300

    login

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import Router from './router';
+//import reportWebVitals from './reportWebVitals';
+
+//Router应用
+ReactDOM.render(
+  <React.StrictMode>
+    {/*  Router应用 */}
+    <Router/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+//setInterval(tick, 1000);
+
